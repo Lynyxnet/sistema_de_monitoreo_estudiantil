4b19a0b539d61a1e20049fa3981561fad77f6b41
lynyx <lynyx@gmail.com>
2024-04-13 15:10:46 -0600

    v1.0.4

--- pages/leer_excel.php
+++ pages/leer_excel.php
@@ -22,71 +22,30 @@ if(isset($_POST['archivoExcel'])){
     //echo $spreadSheetArray[$i][0] . "<br>";
     //La filas seran recorridas con el for mediante la variable $i, y el valor de las columnas queda estatica en la columna 0
     $matricula = $spreadSheetArray[$i][0]; //esta variable guarda el array quien contiene lo valores de cada celda del excel obtenido mendiate el recorrido con el for(){}
+    
+    $success = false;
+
     if(!empty($matricula)){
-      $query_check = "SELECT COUNT(*)"
-
-
-      //$query = "INSERT INTO usuarios (matricula) VALUES(:matricula)";
-      //$stmt = $conn->prepare($query);
-      //if($stmt->execute([':matricula' => $matricula])){
-        echo "Exito! Datos del Excel importados a la base de datos";
-      //}
-      
-    } else  {
-      echo "Error en la insersion";
+      $query_check = "SELECT COUNT(*) FROM usuarios WHERE matricula = :matricula";
+      $stmt_check = $conn->prepare($query_check);
+      $stmt_check->execute([':matricula' => $matricula]);
+      $count = $stmt_check->fetchColumn();
+        if($count > 0){
+          echo "La matricula ya existe en la base de datos";
+        } else {
+            $query_insert = "INSERT INTO usuarios (matricula) VALUES(:matricula)";
+            $stmt_insert = $conn->prepare($query_insert);
+              if($stmt->execute([':matricula' => $matricula])){
+                // echo "Exito! Datos del Excel importados a la base de datos";
+                $success = true;
+              } else {
+                echo "Error en la insersion";
+              }
+        }
+    } else {
+      echo "Problemas al importar el archivo Excel";
     }
   }
-
-}else{
-  echo "Problema al importar el archivo Excel";
 }
 
-
-// $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-// $reader->setReadDataOnly(true);
-// $spreadsheet = $reader->load("archivo.xlsx");
-
-// $sheet = $spreadsheet->getSheet($spreadsheet->getFirstSheetIndex());
-// $data = $sheet->toArray();
-// $sheetCount = count($data);
-//print_r($data);
-
-//Array multidimensional
-// for($c=0; $c <= $sheetCount-1; $c++){
-//   for($f=0; $f <= $sheetCount-1; $f++){
-//     echo $data[$f][$c] . "<br>";
-//   }
-// }
-
 ?>
-
-<!-- 
-Array multidimensional
-
-	     column 0 column1 column2 
-row 0  x[0][0]	x[0][1] x[0][2]   
-row 1  x[1][0]	x[1][1] x[1][2]
-row 3  x[2][0]  x[2][1] x[2][2]
-
-$myarray = array(
-  array("Anna", "Luz", "Ivette"),
-  array("Omar", "Emmanuel", "Hugo")
-)
-
-print_r("$myarray");
- -->
-
- <!DOCTYPE html>
- <html lang="en">
- <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Importar un archivo excel</title>
- </head>
- <body>
-    <form action="#" method="post">
-      <input type="file" name="archivoExcel" id="filename" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"><br>
-      <input type="submit" name="submit">
-    </form>
- </body>
- </html>
