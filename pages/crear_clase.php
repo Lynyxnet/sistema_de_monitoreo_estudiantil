3d8ad5d073303c440731f0b1e3146ca9a023f6ad
lynyx <lynyx@gmail.com>
2024-04-15 01:26:32 -0600

    v1.0.5

--- pages/crear_clase.php
+++ pages/crear_clase.php
@@ -12,12 +12,13 @@ if(!empty($_POST['asignatura'] && !empty($_POST['semestre']))){ //Si es diferent
     $usuario = $_SESSION['usuario'];
     //$archivo = $_POST['archivo']; //Variable con el nombre del archivo //ejemplo.xlsx
     // otra rutina para obtener los datos d elso alumnos del archivo
-    // re;iazo los isert de cada usuario para matricularlo en dicha materia
+    // realiazo los insert de cada usuario para matricularlo en dicha materia
     // $matricula = $_POST['matricula'];
     $semestre = $_POST['semestre'];
     $fecha_inicio = $_POST['fechaInicio'];
     $fecha_final = $_POST['fechaFinal'];
 
+        //Comprobar si existe el curso
         $stmt = $conn->prepare("SELECT * FROM materia WHERE idUsuario = :idusuario AND nombreMateria = :asignatura");
         $stmt->execute([':idusuario' => $usuario, ':asignatura' => $asignatura]);
         $row = $stmt->fetch(); //Obtengo lo valores por medio del fecth y el array lo guardo en la variable $row que contiene todos los datos del usuario consultado
@@ -27,16 +28,17 @@ if(!empty($_POST['asignatura'] && !empty($_POST['semestre']))){ //Si es diferent
             echo "<script> alert('Ya existe el curso en tu catalogo'); window.location.href='docente.php'; </script>";
 
         } else {
+            
             //Crear curso
-            // $idusuario = $row['idUsuario']; //Al consultar por matricula, arrastramos lo datos obtenidos como el idUsuario de la matricula consultado y lo guardamos en la variable $idusuario
-
-            $query = "INSERT INTO materia (nombreMateria, idUsuario, semestre)
-                    VALUES(:asignatura, :idusuario, :semestre)";
+            $query = "INSERT INTO materia (nombreMateria, idUsuario, semestre, fechaInicio, fechaFinal)
+                    VALUES(:asignatura, :idusuario, :semestre, :fechaInicio, :fechaFinal)";
             $stmt = $conn->prepare($query);
             $stmt->execute([
                 ':asignatura' => $asignatura,
                 ':idusuario' => $usuario,
                 'semestre' => $semestre,
+                'fechaInicio' => $fecha_inicio,
+                'fechaFinal' => $fecha_final
             ]);
 
             echo "<script> alert('Creado existosamente'); window.location.href='docente.php'; </script>";
@@ -44,7 +46,9 @@ if(!empty($_POST['asignatura'] && !empty($_POST['semestre']))){ //Si es diferent
         
 } elseif(!empty($_POST['archivoExcel'])){ //Si entra en la condicion es porque tiene el excel cargado, sino la condicion se rompe
     
-    echo "Enviado desde excel";
+    // echo "Enviado desde excel";
+
+    
 
 } else {
     
